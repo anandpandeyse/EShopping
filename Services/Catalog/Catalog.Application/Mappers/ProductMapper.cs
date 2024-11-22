10e97657779603056ce29939170f9127f020f1eb
anandpandeyse <anandpandeyse@hotmail.com>
2024-11-23 02:46:33 +0530

    Code Updated

---
+++ Services/Catalog/Catalog.Application/Mappers/ProductMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace Catalog.Application.Mappers
+{
+    public static class ProductMapper
+    {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfig =>
+            {
+                cfig.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfig.AddProfile<ProductMappingProfile>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+
+        public static IMapper Mapper => Lazy.Value;
+    }
+}
